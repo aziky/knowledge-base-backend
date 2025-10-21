92cb523efbf6d82e4759a3adb3de179532ce8505
aziky <pcm230304@gmail.com>
2025-10-21 10:37:04 +0700

    add extract information from pdf

---
+++ chat-service/api/document_controller.py
@@ -0,0 +1,38 @@
+from flask_restx import Resource, fields, Namespace
+
+from services.document_service import DocumentService
+
+document_service = DocumentService()
+
+
+def document_controller(api):
+    
+    document_ns = api.namespace('Document', 
+                                description='Quản lý các tài liệu kiến thức', 
+                                path='/api/documents')
+    
+    document_model = document_ns.model('Document', {
+        'document_id': fields.String(description='Document ID'),
+        'name': fields.String(description='Document name'),
+        'uploaded_by': fields.String(description='Người upload'),
+        'uploaded_at': fields.DateTime(description='Upload time')
+    })
+
+
+    @document_ns.route('/') 
+    # @document_ns.marshal_list_with(document_model)
+    class DocumentResource(Resource):
+        def get(self):
+            documents = document_service.get_all_documents()
+            result = []
+            for doc in documents:
+                result.append({
+                    'document_id': str(doc.document_id),
+                    'name': doc.name,
+                    'uploaded_by': doc.uploaded_by,
+                    'uploaded_at': doc.uploaded_at.isoformat() if doc.uploaded_at else None
+                })
+            return result, 200
+    
+    return document_ns
+
--- chat-service/main.py
+++ chat-service/main.py
@@ -1,9 +1,13 @@
 import threading
 import os
 from dotenv import load_dotenv
+from flask import app
+from config.config import create_app
 from services.sqs_listener import SQSListener
+from api.chat_controller import chat_controller
+from api.document_controller import document_controller
 
-def main():
+def start_listeners():
     load_dotenv()
     document_queue_url= os.getenv("SQS_DOCUMENT_QUEUE_URL")
     video_queue_url= os.getenv("SQS_VIDEO_QUEUE_URL")
@@ -17,8 +21,26 @@ def main():
     document_thread.start()
     video_thread.start()
     
-    document_thread.join()
-    video_thread.join() 
+    print("SQS listeners started in background...")
+
+
+def main():
+    load_dotenv()
+    
+    app, api = create_app()
+    
+    chat_namespace = chat_controller(api)
+    api.add_namespace(chat_namespace)
+    
+    document_name = document_controller(api)
+    api.add_namespace(document_name)
+    
+    # api.add_namespace(document_controller(api)) 
+    
+    start_listeners()
+
+    port = int(os.getenv("API_PORT", 7075))
+    app.run(host="0.0.0.0", port=port, debug=True, use_reloader=False)
     
 if __name__ == "__main__":
     main()
